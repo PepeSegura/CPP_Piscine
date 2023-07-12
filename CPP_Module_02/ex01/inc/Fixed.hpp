d4750b8d150e508d6668ae40b4b13b2335b7ca7e
Pepe Segura <pepeseguracarreno@gmail.com>
2023-07-13 01:32:58 +0200

    Working on ex01.

---
+++ CPP_Module_02/ex01/inc/Fixed.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pepe <pepe@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 19:08:22 by psegura-          #+#    #+#             */
+/*   Updated: 2023/07/13 01:30:48 by pepe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(Fixed const &f);
+        Fixed(const int int_nb);
+        Fixed(const float float_nb);
+        Fixed&  operator=(Fixed const &f);
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+        float   toFloat(void) const;
+        int     toInt(void) const;
+    private:
+        int                 fixed;
+        static const int    bits = 8;
+};
+
+std::ostream& operator<<(std::ostream &out, Fixed const &f);
---
+++ CPP_Module_02/ex01/srcs/Fixed.cpp
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pepe <pepe@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 19:23:08 by psegura-          #+#    #+#             */
+/*   Updated: 2023/07/13 01:31:35 by pepe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed:: Fixed()
+{
+    std::cout << "Default constructor called." << std::endl;
+    this->fixed = 0;
+}
+
+Fixed:: ~Fixed()
+{
+    std::cout << "Destructor called." << std::endl;
+}
+
+Fixed:: Fixed(const Fixed& f)
+{
+    std::cout << "Copy constructor called." << std::endl;
+    fixed = f.getRawBits();
+}
+
+Fixed:: Fixed(const int int_nb)
+{
+    std::cout << "INT constructor called." << std::endl;
+    this->fixed = int_nb;
+}
+
+Fixed:: Fixed(const float float_nb)
+{
+    std::cout << "FLOAT constructor called." << std::endl;
+    (void)float_nb;
+}
+
+int Fixed:: getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->fixed);
+}
+
+void Fixed:: setRawBits(int const raw)
+{
+    this->fixed = raw;
+}
+
+float Fixed:: toFloat(void) const
+{
+    float result = 0;
+
+    return (result);
+}
+
+int Fixed:: toInt(void) const
+{
+    int result = 0;
+
+    return (result);
+}
+
+Fixed& Fixed:: operator=(const Fixed& f)
+{
+    std::cout << "Asignation operand called." << std::endl;
+    if (this != &f)
+    {
+        fixed = f.getRawBits();
+    }
+    return (*this);
+}
+
+std::ostream& operator<<(std::ostream &out, Fixed const &f)
+{
+    float result;
+
+    result = f.toFloat();
+    out << result;
+    return (out);
+}
